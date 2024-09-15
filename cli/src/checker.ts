06c6874821bf0571c61add63ca86c06c476f27b3
Wes Johnson <wes@swj.io>
2024-09-15 16:23:47 -0400

    fix shasum extraction for podfile check

--- cli/src/checker.ts
+++ cli/src/checker.ts
@@ -44,7 +44,8 @@ export const checkLockfilesAndExit = async ({
   const podfileChecksum = await getPodLockfileStableChecksum(project);
   const gradleChecksum = await $`shasum android/app/gradle.lockfile`;
 
-  const podfileChecksumMatch = podfileChecksum === basePodfileChecksum;
+  const podfileChecksumMatch =
+    shasumHash(podfileChecksum) === shasumHash(basePodfileChecksum);
   const gradleChecksumMatch =
     shasumHash(gradleChecksum) === shasumHash(baseGradleChecksum);
 
