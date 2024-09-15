f3c4e543ac2c8a4677d3b842a1f20a9cad178c4d
Wes Johnson <wes@swj.io>
2024-09-15 09:06:43 -0400

    fix dep resolution

--- tests/setup.sh
+++ tests/setup.sh
@@ -11,5 +11,7 @@ yarn prepublishOnly
 cp -r example test-example
 
 cd test-example && yarn add ../
+# make sure we resolve to the correct shared dependency
+rm -rf node_modules/expo-native-lockfiles/node_modules
 
 echo "Finished test env setup ===================="
